6ba145a0d4ff86c4a0247ba6f3f99d8b12e173d0
Bernardo <bmgg358@gmail.com>
2016-10-23 23:22:19 -0400

    first commit

---
+++ server/boot/mongo.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(app) {
+  /*app.dataSources.mongo.automigrate('materia', function(err) {
+    if (err) throw err;
+    app.models.materia.create([{
+      nombre: 'Base de Datos',
+      codigo: 'FPTSP01',
+      created: Date(),
+      modified: Date()},
+      {nombre: 'Ingeniería de Software',
+      codigo: 'FPTSP07',
+      created: Date(),
+      modified: Date()},
+      {nombre: 'Sistemas de Información',
+      codigo: 'FPTSP04',
+      created: Date(),
+      modified: Date()}],
+      function(err, materia) {
+        if (err) throw err;
+        console.log('Models created: \n', materia);
+      });
+  });*/
+};
